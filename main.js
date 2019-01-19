0040f0a36418eaedc8fc92f9556a3e0ad274ac89
Charles <ellerkaresz@gmail.com>
2019-01-19 15:34:35 +0100

    changes made are saved and can be viewed

--- main.js
+++ main.js
@@ -52,25 +52,62 @@ class Database {
       static addItem(item) {
             firebase.firestore().collection('items').add(item).then(() => {
                   M.toast({html: 'Sikeresen hozzáadva', classes: 'green'});
+                  Database.addToChanges(item, 'add');
             }).catch(() => {
                   M.toast({html: 'Sikertelen hozzáadás', classes: 'red'});
             });
       }
 
       static deleteItem(itemId){
+            const changedItem = document.getElementById(itemId).parentElement.children;
             firebase.firestore().collection('items').doc(itemId).delete().then(() => {
                   M.toast({html: 'Sikeresen törölve', classes: 'green'});
+                  Database.addToChanges(changedItem, 'delete');
             }).catch(() => {
                   M.toast({html: 'Sikertelen törlés', classes: 'red'});
             });
       }
 
       static updateItem(itemId, amount){
+            const changedItem = document.getElementById(itemId).parentElement.children;
             firebase.firestore().collection('items').doc(itemId).update({ amount }).then(() => {
                   M.toast({html: 'Sikeresen szerkesztve', classes: 'green'});
+                  Database.addToChanges(changedItem, 'update', amount);
             }).catch(() => {
                   M.toast({html: 'Sikertelen szerkesztés', classes: 'red'});
             });
+      }
+
+      static getChanges(limit){
+            firebase.firestore().collection('changes').orderBy('date', 'desc').limit(limit).onSnapshot(snapshot => {
+                  let changes = document.getElementById('changes-tbody');
+                  changes.innerHTML = '';
+                  snapshot.docs.forEach(doc => {
+                        let data = doc.data();
+                        changes.innerHTML += `<tr>
+                              <td>${data.name}</td><td>${data.type}</td><td>${data.id}</td><td>${data.amount}</td><td>${data.date}</td>
+                        </tr>`;
+                  });
+            });
+      }
+
+      static addToChanges(item, action, amount){
+            const date = new Date().toISOString().split('T');
+            firebase.firestore().collection('changes').add(
+                  action === 'add' ? {
+                        name: item.name,
+                        type: item.type,
+                        id: item.id,
+                        amount: `<span class="green-text">Hozzáadva (${item.amount})</span>`,
+                        date: `${date[0]} ${date[1].split('.')[0]}`
+                  } : {
+                        name: item[0].innerHTML,
+                        type: item[1].innerHTML,
+                        id: item[2].innerHTML,
+                        amount: action === 'update' ? `<span class="red-text">${item[3].children[0].innerHTML}</span> → <span class="green-text">${amount} db</span>` : '<span class="red-text">Törölve</span>',
+                        date: `${date[0]} ${date[1].split('.')[0]}`
+                  }
+            );
       }
 }
 
@@ -82,13 +119,14 @@ document.addEventListener('DOMContentLoaded', () => {
       firebase.initializeApp({apiKey: "AIzaSyB5Mei15xp6ykQUV2p59K1j8lrDk5jsFEI", authDomain: "precise-elektrik.firebaseapp.com", databaseURL: "https://precise-elektrik.firebaseio.com", projectId: "precise-elektrik"});
 
       Database.getItems();
+      Database.getChanges(5);
 });
 
 const itemForm = document.getElementById('item-form');
 itemForm.addEventListener('submit', (e) => {
       e.preventDefault();
 
-      const group = itemForm['item-group-input'].value;
+      const group = itemForm['item-group-input'].value.trim();
       const name = itemForm['item-name-input'].value;
       const type = itemForm['item-type-input'].value;
       const id = itemForm['item-id-input'].value;
@@ -105,7 +143,7 @@ document.addEventListener('click', (e) => {
       }
 
       if(e.target.classList.contains('edit')){
-            amountForm.id = e.target.parentElement.id;
+            amountForm.dataset.id = e.target.parentElement.id;
             amountForm['edit-amount-input'].value = e.target.innerHTML.split(' ')[0];
       }
 
@@ -119,6 +157,6 @@ const amountForm = document.querySelector('.edit-amount-form');
 amountForm.addEventListener('submit', (e) => {
       e.preventDefault();
 
-      Database.updateItem(e.target.id, e.target['edit-amount-input'].value);
+      Database.updateItem(e.target.dataset.id, e.target['edit-amount-input'].value);
       amountForm.reset();
 });
